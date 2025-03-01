e8338a073e1ad22e46f748d10ab93fc3a7916f97
aza-bi <amuhitdinov69@gmail.com>
2025-03-01 12:42:02 +0500

    first commit

---
+++ Users.py
@@ -0,0 +1,7 @@
+class Users:
+    def __init__(self, name: str, phone_number: str, email: str, yearly_income:float, password:str):
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        self.yearly_income = yearly_income
+        self.password = password
